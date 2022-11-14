1f291717304240d70b749cb8a71b911668c7b807
joaovq <vitorsnta90@gmail.com>
2022-11-14 17:13:02 -0300

    list exercises

--- src/main/kotlin/exercises_santander_coders/lista_de_exercicios/Questao1.kt
+++ src/main/kotlin/exercises_santander_coders/lista_de_exercicios/Questao1.kt
@@ -7,16 +7,19 @@ fun main(args: Array<String>) {
     for (i in 0 until 10){
         println("Digite o número ${i+1}:")
         num = readln().toInt()
+//        Validação
         if (i==0) {
              maior = num
              menor = num
         }
+//        Código do maior
         maior = if (num > maior){
             num
         }
         else{
             maior
         }
+//        Código do menor
         menor = if (num < menor){
             num
         }
