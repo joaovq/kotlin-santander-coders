ae8703e39a35e7c95ed7f5c8f00086b54af39018
joaovq <vitorsnta90@gmail.com>
2022-11-16 12:43:18 -0300

    Correção questão 5 para números primos.

--- src/main/kotlin/exercises_santander_coders/lista_de_exercicios/Questao5.kt
+++ src/main/kotlin/exercises_santander_coders/lista_de_exercicios/Questao5.kt
@@ -3,6 +3,7 @@ package exercises_santander_coders.lista_de_exercicios
 fun main(args: Array<String>) {
     println("Digite um número máximo: ")
     val maxNumber = readln().toInt()
+    val listNumPrimos  : MutableList<Int> = mutableListOf()
     if (maxNumber<1){
         println("Número inválido, repita a execução do programa!")
         return
@@ -13,8 +14,15 @@ fun main(args: Array<String>) {
 
    for (i in 1..maxNumber){
        if ((2 until i).none { i % it == 0 })
-           println(i)
-
+            listNumPrimos.add(i)
    }
 
+    println(listNumPrimos)
+//    Utilizando o método filter
+
+    val listNumPrimosFilter = (1..maxNumber).
+                                        filter { (2 until it).
+                                        none { num -> num % it == 0 } }
+
+    println("Pelo método filter $listNumPrimos")
 }
