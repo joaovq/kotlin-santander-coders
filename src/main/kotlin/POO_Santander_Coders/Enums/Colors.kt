f3b0bf6f019b42c1ff09fd86ede86afe47715265
joaovq <vitorsnta90@gmail.com>
2022-12-01 00:32:03 -0300

    Exercicio Enum Colors.kt

--- src/main/kotlin/POO_Santander_Coders/Enums/Colors.kt
+++ src/main/kotlin/POO_Santander_Coders/Enums/Colors.kt
@@ -4,14 +4,31 @@ import org.w3c.dom.css.RGBColor
 import javax.swing.plaf.synth.ColorType
 
 enum class Color(val nameColor:String,val hex:String,val rgb:String){
-        VERMELHO(nameColor = "Vermelho", hex = "#eb4034", rgb = "235, 64, 52"),
-        BRANCO(nameColor = "Branco", hex = "#ffffff", rgb = "255, 255, 255"),
-        PRETO(nameColor = "Preto", hex = "#000000", rgb = "0, 0, 0"),
-        VERDE(nameColor = "Verde", hex = "#4dff00", rgb = "77, 255, 0");
+        VERMELHO(nameColor = "Vermelho", hex = "#eb4034", rgb = "235, 64, 52") {
+                override fun transformarNomeEmUpperCase():String {
+                        return "VERMELHO"
+                }
+        },
+        BRANCO(nameColor = "Branco", hex = "#ffffff", rgb = "255, 255, 255") {
+                override fun transformarNomeEmUpperCase() :String{
+                        return "Branco"
+                }
+        },
+        PRETO(nameColor = "Preto", hex = "#000000", rgb = "0, 0, 0") {
+                override fun transformarNomeEmUpperCase() :String{
+                        return "PRETO"
+                }
+        },
+        VERDE(nameColor = "Verde", hex = "#4dff00", rgb = "77, 255, 0") {
+                override fun transformarNomeEmUpperCase():String {
+                        return "VERDE"
+                }
+        };
 
         override fun toString(): String {
             return "Color(nameColor='$nameColor', hex='$hex', rgb='$rgb')"
         }
+        abstract fun transformarNomeEmUpperCase() :String
 
 }
 
@@ -27,9 +44,9 @@ fun main(args: Array<String>) {
         println(Color.VERMELHO)
         println(Color.VERDE)
         println(Color.PRETO)
-        println(Status.ONLINE)
 //        Status Enum
         println(Status.ONLINE.ordinal)
+        println(Status.ONLINE)
         println(Status.OFFLINE.ordinal)
         println(Status.DORMINDO.ordinal)
 
