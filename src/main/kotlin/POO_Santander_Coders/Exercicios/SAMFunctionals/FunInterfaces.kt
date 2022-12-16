8fc800b77720e2ca2a174bf74ec1115b7da57123
joaovq <vitorsnta90@gmail.com>
2022-12-16 12:48:14 -0300

    SamFunctionals FunInterfaces.kt

---
+++ src/main/kotlin/POO_Santander_Coders/Exercicios/SAMFunctionals/FunInterfaces.kt
@@ -0,0 +1,23 @@
+package POO_Santander_Coders.Exercicios.SAMFunctionals
+
+
+fun interface IntPredicate {
+    fun accept(i: Int): Boolean
+
+//    fun isOdd():Boolean
+}
+////Utilizando mais de um método precisamos declarar mais de um objeto e implementar as funcoes
+//val isEven = object :IntPredicate{
+//    override fun accept(i: Int): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//    override fun isOdd():Boolean {
+//        TODO("Not yet implemented")
+//    }
+//}
+
+val isEven = IntPredicate { it % 2 == 0 }
+
+fun main() {
+    println("Is 7 even? - ${isEven.accept(7)}")
+}
