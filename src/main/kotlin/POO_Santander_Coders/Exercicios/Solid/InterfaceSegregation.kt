fa3bdd6cab52e75b0c9e071ee175ea9905a7ce6b
joaovq <vitorsnta90@gmail.com>
2022-12-16 13:40:47 -0300

    Example Robot InterfaceSegregation.kt.kt in SolidConcepts

--- src/main/kotlin/POO_Santander_Coders/Exercicios/Solid/InterfaceSegregation.kt
+++ src/main/kotlin/POO_Santander_Coders/Exercicios/Solid/InterfaceSegregation.kt
@@ -10,3 +10,42 @@ fun interface DataSource{
 
 //Cada função podem implementar uma interface funcional também
 internal fun createDataSource(): DataSource = TODO("Provide a datasource")
+
+
+//Outro Exemplo
+
+
+abstract class Robot(
+    open var nameRobot:String,
+    open var color:String
+)
+
+data class SimpleRobot(
+    override var nameRobot: String,
+    override var color: String,
+    override val aim: Int
+): Robot(nameRobot, color),
+    RobotAim {
+    override fun aimRobot() {
+        TODO("Not yet implemented")
+    }
+}
+
+//Neste caso utilizar se todas as outras interfaces tiverem algo em comum
+interface IRobot{
+}
+
+interface RobotAim:IRobot{
+    val aim:Int
+    fun aimRobot()
+}
+
+interface RobotSpin{
+    val rotation:Int
+    fun rotateArms()
+}
+
+interface RobotAntenna{
+    val widthAntenna:Int
+    fun wiggleAntenna()
+}
