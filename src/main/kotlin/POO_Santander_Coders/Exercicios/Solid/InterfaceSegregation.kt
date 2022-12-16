55a4ccd9770197e590d65caab9e5a36afc83150b
joaovq <vitorsnta90@gmail.com>
2022-12-16 13:07:10 -0300

    InterfaceSegregation.kt.kt in SolidConcepts

---
+++ src/main/kotlin/POO_Santander_Coders/Exercicios/Solid/InterfaceSegregation.kt
@@ -0,0 +1,12 @@
+package POO_Santander_Coders.Exercicios.Solid
+
+fun main(args: Array<String>) {
+
+}
+
+fun interface DataSource{
+    fun findById()
+}
+
+//Cada função podem implementar uma interface funcional também
+internal fun createDataSource(): DataSource = TODO("Provide a datasource")
