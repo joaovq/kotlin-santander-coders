809c3c8ff4296a7663cba943abb0983b777157ce
João Vítor <vitorsnta90@gmail.com>
2022-12-26 21:14:01 -0300

    in progress ListaExercicio1.kt

--- src/main/kotlin/EstruturaDeDados_Santander_Coders/Exercicios/Listas/ListaExercicio1.kt
+++ src/main/kotlin/EstruturaDeDados_Santander_Coders/Exercicios/Listas/ListaExercicio1.kt
@@ -1,5 +1,15 @@
 package EstruturaDeDados_Santander_Coders.Exercicios.Listas
 
+
+/*
+*
+*
+* TODO:Dado a estrutura de lista ligada que construímos em sala, adicionar:
+*  -Método de busca por um valor específico
+*  -Otimização para termos a adição de um nó ao final de O(1),
+*  ou seja, será necessário manter uma referência para o último elemento da lista
+*
+* */
 data class Node<T>(
     var value: T,
     var next: Node<T>?
@@ -8,23 +18,63 @@ data class Node<T>(
 data class LinkedList2<T>(
     var head: Node<T>? = null
 ) {
+    private var last:Node<T>? = null
     fun add(value: T) {
         val newNode = Node(value = value, null)
 
         if (head == null) {
             head = newNode
+            last = newNode
             return
         } else {
-            var current = head
-            while (current?.next != null) {
-                current = current.next
-            }
+//            var current = head
+//            while (current?.next != null) {
+//                current = current.next
+//            }
+            val current = last
             current?.next = newNode
+            last = newNode
         }
     }
 
-    fun getNode(node: Node<T>){
+    fun getIndexAt(searchedValue: T):Int?{
+        var current = head
+        var currentIndex: Int  = 0
+        if (current == null){
+            println("Lista vazia")
+            return null
+        }
 
+        do{
+            if (current?.value==searchedValue) {
+                return currentIndex
+            }
+            current = current?.next
+            currentIndex++
+        }
+        while (current!=null)
+
+        println("Elemento procurado não existe dentro da lista")
+        return null
+    }
+
+    fun getNode(searchedValue: T):Node<T>?{
+        var current = head
+        if (current == null){
+            println("Lista vazia")
+            return null
+        }
+
+        do{
+            if (current?.value==searchedValue) {
+                return current
+            }
+            current = current?.next
+        }
+        while (current!=null)
+
+        println("Elemento procurado não existe dentro da lista")
+        return null
     }
 
     fun remove(value: T): Boolean {
@@ -64,4 +114,25 @@ data class LinkedList2<T>(
     fun isEmpty(): Boolean {
         return head == null
     }
+}
+
+fun main(args: Array<String>) {
+    val listOfInts = LinkedList2<Int>()
+
+    listOfInts.add(1)
+    listOfInts.add(2)
+    listOfInts.add(2)
+    listOfInts.add(3)
+
+    listOfInts.show()
+
+    listOfInts.remove(2)
+    listOfInts.show()
+
+    val indexOfSearched = listOfInts.getIndexAt(3)
+    val nodeResult = listOfInts.getNode(2)
+
+    println(nodeResult)
+
+    println(indexOfSearched)
 }
