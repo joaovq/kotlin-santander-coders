f0ef1fe3647fd1e79172aeb6338e7bbd4fd1b163
joaovq <vitorsnta90@gmail.com>
2023-01-07 12:18:55 -0300

    Concluide MensageriaEx1.kt

--- src/main/kotlin/estruturaDeDados_Santander_Coders/exercicios/FilasEPilha/MensageriaEx1.kt
+++ src/main/kotlin/estruturaDeDados_Santander_Coders/exercicios/FilasEPilha/MensageriaEx1.kt
@@ -17,24 +17,50 @@ import kotlin.random.Random
 * */
 data class Message(
     val text: String, val recipient: String
-)
+){
+    var sendAttempt:Int = 0
+}
 
 class MessageBroker {
-    private val messageSender = Random(1000)
+    private val messageSender = Random(System.currentTimeMillis())
     val queue: Queue<Message> = LinkedList()
-    fun processFirstMessage() {/*
+    fun processFirstMessage() {
+        /*
         * Implementar maneira de processar a fila e de retentar o envio de uma mensagem que deu erro
         */
         //send(message)
 //        Pega o ultimo mas não remove
         val lastMessageInQueue = queue.peek()
         val send = send(lastMessageInQueue)
-        if (send) {
+        if (send && lastMessageInQueue.sendAttempt<3) {
+            println("""
+                
+                Mensagem enviada com sucesso.
+                 
+                 Text: ${lastMessageInQueue.text}
+                 Recipient: ${lastMessageInQueue.recipient}
+                 Attempt: ${lastMessageInQueue.sendAttempt}
+                 
+                 
+                 """.trimIndent())
             queue.poll()
-        } else {
+        }else if(lastMessageInQueue.sendAttempt==3){
+            println("""
+                
+                Tentativa de envio foi abortada.
+                 
+                 Text: ${lastMessageInQueue.text}
+                 Recipient: ${lastMessageInQueue.recipient}
+                 
+                 
+                 """.trimIndent())
+            queue.poll()
+        }
+        else {
 //            Remove o último e pega
-            val messageRemoved = queue.poll()
-            queue.add(lastMessageInQueue)
+            queue.poll()
+            lastMessageInQueue.sendAttempt++
+            queue.offer(lastMessageInQueue)
         }
     }
 
@@ -44,4 +70,26 @@ class MessageBroker {
     private fun send(message: Message): Boolean {
         return messageSender.nextBoolean()
     }
+}
+
+
+fun main(args: Array<String>) {
+    val message1 = Message("Hello car", "joshue@jsn.com")
+    val message2 = Message("Please give the cart", "gabriel@jmsa.com")
+    val message3 = Message("Hey man, what's up?", "hesketh69@hotmail.com")
+    val message4 = Message("Wait one minute, please", "Hamg_333@gmail.com")
+
+
+    val messageBroker = MessageBroker()
+    messageBroker.queue.offer(message1)
+    messageBroker.queue.offer(message2)
+    messageBroker.queue.offer(message3)
+    messageBroker.queue.offer(message4)
+
+    with(messageBroker){
+        while (this.queue.isNotEmpty()){
+            this.processFirstMessage()
+        }
+    }
+
 }
