3a1fc7562c8a135ba0e1f41abe4d19f2709b9e46
joaovq <vitorsnta90@gmail.com>
2023-01-18 19:38:31 -0300

    aula5e6

--- src/main/kotlin/estruturaDeDados_Santander_Coders/aula5eaula6/TreeSemUsarOArray.kt
+++ src/main/kotlin/estruturaDeDados_Santander_Coders/aula5eaula6/TreeSemUsarOArray.kt
@@ -3,7 +3,15 @@ package estruturaDeDados_Santander_Coders.aula5eaula6
 import kotlin.math.abs
 import kotlin.math.max
 
-data class Node2<T>(val value:T, var left:Node2<T>? = null, var right:Node2<T>?=null, var height:Int = 0){
+
+
+data class Node2<T>(
+    val value: T,
+    var left: Node2<T>? = null,
+    var right: Node2<T>? = null,
+    var height: Int = 1
+) {
+
     fun isLeaf(): Boolean {
         return left == null && right == null
     }
@@ -38,128 +46,138 @@ data class Node2<T>(val value:T, var left:Node2<T>? = null, var right:Node2<T>?=
     }
 }
 
-class Tree2<T:Comparable<T>> {
+fun <T> Node2<T>?.height(): Int {
+    return this?.height ?: 0
+}
+
+class Tree2<T : Comparable<T>> {
+
     private var root: Node2<T>? = null
 
-
-    fun isBalanced(node: Node2<T>? = root):Int{
+    /**
+     * Se retornar >= 0 está balanceada
+     * Caso contrário não está balanceada
+     */
+    fun isBalanced(node: Node2<T>? = root): Int {
         if (node == null) return 0
 
-        val leftHeight  = isBalanced(node.left)
-
-        if (leftHeight < 0) return -1
+        val leftHeight = isBalanced(node.left)
+        if (leftHeight < 0)
+            return -1
 
         val rightHeight = isBalanced(node.right)
+        if (rightHeight < 0)
+            return -1
 
-        if (rightHeight < 0) return -1
-
-        return if (abs(leftHeight-rightHeight) <=1){
-    //            Está balanceado
-            max(leftHeight, rightHeight)+1
-        } else{
-            -1
+        if (abs(leftHeight - rightHeight) <= 1) {
+            // está balanceado
+            return max(leftHeight, rightHeight) + 1
+        } else {
+            return -1
         }
     }
 
-    fun rotateLeft(node:Node2<T>): Node2<T>?{
-        val right = node.right
-        val leftRightNode = right?.left
+    fun rotateLeft(node: Node2<T>): Node2<T>? {
+        println("Rotate left " + node.value)
+        val rightNode = node.right
+        val leftOfRightNode = rightNode?.left
 
-        right?.left = node
-        node.right = leftRightNode
-        return right
+        rightNode?.left = node
+        node.right = leftOfRightNode
+
+        node.height = max(node.left.height(), node.right.height()) + 1
+        rightNode?.height = max(rightNode?.left.height(), rightNode?.right.height() )
+
+        return rightNode
     }
-    fun rotateRight(node: Node2<T>):Node2<T>?{
-        val leftNode = node.left
-        val rightLeftNode = leftNode?.right
 
+    fun rotateRight(node: Node2<T>): Node2<T>? {
+        println("Rotate right " + node.value)
+        val leftNode = node.left
+        val rightOfLeftNode = leftNode?.right
 
         leftNode?.right = node
-        node.left = rightLeftNode
+        node.left = rightOfLeftNode
+
+        node.height = max(node.left.height(), node.right.height())
+        leftNode?.height = max(leftNode?.left.height(), leftNode?.right.height())
 
         return leftNode
     }
-    fun add(value:T){
-        if (root == null){
+
+    fun add(value: T) {
+
+        if (root == null) {
             root = Node2(value)
-        }else{
-            add(root!!, value)
+        } else {
+            root = add(root, value)
         }
     }
 
-    private fun add(node: Node2<T>?, value: T):Node2<T> {
-        if (node == null) return Node2(value)
+    private fun add(node: Node2<T>?, value: T): Node2<T>? {
+        if (node == null)
+            return Node2(value)
 
-//Para a arvore AVL
-        if (node.value> value){
-//            Inserir a esquerda
+        if (node.value > value) {
+            // inserir a esquerda
             node.left = add(node.left, value)
-        }else if (node.value< value){
+        } else if (node.value < value) {
+            // inserir a direita
             node.right = add(node.right, value)
-        }
-        else{
+        } else {
+            // não inserimos valores repetidos
             return node
         }
 
-//        val balance: Int = (node.left?.height() - node.right.height())
+        node.height = 1 + max(node.left.height(), node.right.height())
+
+        val balance = node.left.height() - node.right.height()
+
+        if (balance > 1 && node.left!!.value > value) {
+            return rotateRight(node)
+        }
+
+        if (balance < -1 && node.right!!.value < value) {
+            return rotateLeft(node)
+        }
+
+        if (balance > 1 && node.left!!.value < value) {
+            node.right = rotateLeft(node.left!!.right!!)
+            return rotateRight(node)
+        }
+
+        if (balance < -1 && node.right!!.value > value) {
+            node.left = rotateRight(node.right!!.left!!)
+            return rotateLeft(node)
+        }
 
         return node
-       /* if (node.value >= value) {
-            if (node.left == null) {
-                println("add esquerda ${node.value}")
-                node.left = Node2(value)
-            } else {
-                println("esquerda")
-                add(node.left!!, value)
-            }
-        } else {
-            if (node.right == null) {
-                println("add direita ${node.value}")
-                node.right = Node2(value)
-            } else {
-                println("direita")
-                add(node.right!!, value)
-            }
-        }*/
     }
-//      O(log n)
-//    Precisa de menos tempo para procurar e salvar os dados
+
     fun search(value: T): Boolean {
-        var next = root
-        do {
-            next =
-                when{
-                    next?.value!!>value->{
-                        next.left
-                    }
-                    next.value >value->{
-                        next.left
-                    }
-                    next.value == value->{
-                        return true
-                    }
-                    else -> {
-                        return false
-                    }
-                }
-        }while (next !=null)
+        var node = root
+
+        while (node != null) {
+            when {
+                node.value == value -> return true
+                node.value > value -> node = node.left
+                node.value < value -> node = node.right
+            }
+        }
         return false
     }
 
-
-
-    fun remove(value: T){
+    fun remove(value: T) {
         if (root == null) return
 
-        if (root?.value!!>=value){
-            remove(root!!, root?.left!!, value)
-        }else{
-            remove(root!!, root?.right!!, value)
+        if (root!!.value > value) {
+            remove(root!!, root?.left, value)
+        } else if (root!!.value < value) {
+            remove(root!!, root?.right, value)
         }
-
     }
 
-    private fun remove(parent:Node2<T>, current:Node2<T>?, value: T){
+    private fun remove(parent: Node2<T>, current: Node2<T>?, value: T) {
         if (current == null) return
 
         if (current.value == value) {
@@ -177,7 +195,6 @@ class Tree2<T:Comparable<T>> {
         }
     }
 
-
     override fun toString(): String {
         return toString(root)
     }
@@ -185,24 +202,58 @@ class Tree2<T:Comparable<T>> {
     private fun toString(node: Node2<T>?): String {
         if (node == null) return ""
 
-        return node.value.toString() + " " + toString(node.left) + " " + toString(node.right)
+        return node.value.toString() +
+                " " +
+                toString(node.left) + " " +
+                toString(node.right)
     }
 
+    // In Order, Post Order, Pre Order
+    // In Order = esq nó direita
+    // Post Order = direita nó esquerda
+    // Pre Order = nó esquerda direita
+
+    fun preOrder(node: Node2<T>? = root) {
+        if (node == null) return
+
+        println(node.value)
+        preOrder(node.left)
+        preOrder(node.right)
+    }
+
+    fun inOrder(node: Node2<T>? = root) {
+        if (node == null) return
+
+        inOrder(node.left)
+        println(node.value)
+        inOrder(node.right)
+    }
+
+    fun postOrder(node: Node2<T>? = root) {
+        if (node == null) return
+
+        postOrder(node.right)
+        println(node.value)
+        postOrder(node.left)
+    }
 }
 
+fun main() {
+    val tree = Tree2<Int>()
+    tree.add(8)
+    println(tree.isBalanced()) //true
+    tree.add(5)
+    println(tree.isBalanced()) // true
+    tree.add(4)
+    println(tree.isBalanced())
+    tree.add(10)
+    println(tree.isBalanced())
+    tree.add(9)
 
-fun <T> Node2<T>.height() = this.height
-
-fun main(args: Array<String>) {
-    val tree2 = Tree2<Int>()
-    tree2.add(8)
-    println(tree2.isBalanced())
-    tree2.add(5)
-    println(tree2.isBalanced())
-    tree2.add(4)
-    println(tree2.isBalanced())
-
-    val search = tree2.search(3)
-
-    println(search)
+    println("------------")
+    tree.inOrder()
+    println("------------")
+    tree.postOrder()
+    println("------------")
+    tree.preOrder()
 }
